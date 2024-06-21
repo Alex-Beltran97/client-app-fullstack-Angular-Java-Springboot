b259f93a666760c3ce354481d403338598585e1a
Pedro Beltran <pedroalexander.beltran.contractor.cidentity@bbva.com>
2024-06-21 08:17:28 -0500

    Creating a copy of .jar file

---
+++ server/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.client (address, city, doc_number, doc_type, first_name, last_name, phone, second_last_name,
+                           second_name)
+VALUES ('Calle Falsa 123', 'Bogota', 23445322, 'c', 'Jhenny', 'Perez', '3135555555', 'Garcia', 'Maria');
